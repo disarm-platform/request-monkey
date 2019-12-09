7b0e892a2dec7b1403f4627f7e14a5636c5235ca
superuser-chi <mavusogiftntokozo@gmail.com>
2019-12-09 16:56:26 +0200

    update preprocess params

--- function/preprocess_params.py
+++ function/preprocess_params.py
@@ -1,8 +1,8 @@
-from function.preprocess_helpers import required_exists, is_type
+from function.preprocess_helpers import check_if_exists, is_type
 
 
 def preprocess(params: dict):
-    required_exists('func_name', params)
+    if check_if_exists('func_name', params): 
+        # Optional, but if exists, must be string
+        is_type('func_name', params, str)
 
-    # Optional, but if exists, must be string
-    is_type('func_name', params, str)
